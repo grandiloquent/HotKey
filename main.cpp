9c71ce54027861dad525f07ebde8f84f0d8ff78b
grandiloquent <1785907865@qq.com>
2021-09-21 23:20:27 +0800

    同步

--- main.cpp
+++ main.cpp
@@ -1,26 +1,25 @@
-// --std=c++20  
+// --std=c++20
 
 #include <Windows.h>
+#include <sys/stat.h>
 
 #include <algorithm>
- 
-#include <vector>
+#include <filesystem>
+#include <fstream>
 #include <iostream>
-#include <fstream> 
-#include <sys/stat.h>
-#include <filesystem> 
 #include <thread>
-#include "strings.hpp"
+#include <vector>
+
 #include "scope.hpp"
+#include "strings.hpp"
 #include "clipboard.hpp"
 #include "processes.hpp"
-#include "files.hpp"
 #include "baidu.hpp"
+#include "files.hpp"
 #include "git.hpp"
 #include "https.hpp"
 //#include "rapidjson/document.h"
 //#include "sion.h"
-
 
 using namespace std;
 
@@ -31,10 +30,10 @@ LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam) {
     if (vkCode != 165) std::cout << vkCode << " ";
     switch (vkCode) {
       case 48:  // 0
-       
+
         break;
 
-      case 57: // 9
+      case 57:  // 9
         gitPush();
         break;
     }
@@ -42,20 +41,28 @@ LRESULT CALLBACK HookProc(int nCode, WPARAM wParam, LPARAM lParam) {
   return CallNextHookEx(NULL, nCode, wParam, lParam);
 }
 
-
 int main(int argc, char const* argv[]) {
-  //setlocale(LC_ALL, "zh-CN");
-  //createClangFiles("https");
+  // setlocale(LC_ALL, "zh-CN");
+  // createClangFiles("https");
 
-  cout << Utf8ToGbk("ALT+9 同步GitHub") << endl;
+  wstring value = getString();
+  const wchar_t* wp = value.c_str();
+  char szUTF8[32];
+  int nMbyteLen = WideCharToMultiByte(CP_UTF8, 0, wp, -1, NULL, 0, NULL, NULL);
+  WideCharToMultiByte(CP_UTF8, 0, wp, -1, szUTF8, nMbyteLen, NULL, NULL);
+string encode=szUTF8;
+cout << urlencode( encode) << endl;
 
-  MSG msg;
+cout << Utf8ToGbk("ALT+9 同步GitHub") << endl;
 
-  SetWindowsHookEx(WH_KEYBOARD_LL, HookProc, GetModuleHandle(NULL),0);  // Setup keyboard Hook
+MSG msg;
 
-  while (GetMessage(&msg, 0, 0, 0)) {
-    TranslateMessage(&msg);
-    DispatchMessage(&msg);
+SetWindowsHookEx(WH_KEYBOARD_LL, HookProc, GetModuleHandle(NULL),
+                 0);  // Setup keyboard Hook
+
+while (GetMessage(&msg, 0, 0, 0)) {
+  TranslateMessage(&msg);
+  DispatchMessage(&msg);
   }
   return 0;
 }
